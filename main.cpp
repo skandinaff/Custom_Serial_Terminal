f5df112f180012bea4713d566014cf0f4daf6e3d
skandinaff <sevalks@gmail.com>
2023-09-27 18:23:05 +0300

    now it works in ascyhronous and non blocking way

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 #include <iostream>
 #include <string>
 
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <atomic>
+
 const char* COM_PORT = "COM8";  // Change to the desired COM port
 const int BAUD_RATE = 115200;
 const int BUFFER_SIZE = 256;
+
+std::atomic<bool> shouldExit(false);
+
+void ReadSerialData(HANDLE hSerial) {
+    char buffer[BUFFER_SIZE];
+    DWORD bytesRead;
+
+    while (!shouldExit) {
+        if (ReadFile(hSerial, buffer, BUFFER_SIZE, &bytesRead, nullptr)) {
+            if (bytesRead > 0) {
+                // Print received data to the console
+                std::cout.write(buffer, bytesRead);
+                std::cout.flush();  // Ensure the output is immediately visible
+            }
+        }
+    }
+}
 
 int main() {
     HANDLE hSerial;
@@ -35,31 +58,28 @@ int main() {
         return 1;
     }
 
-    char buffer[BUFFER_SIZE];
-    DWORD bytesRead, bytesWritten;
+    // Create a thread for asynchronous serial data reading
+    std::thread serialThread(ReadSerialData, hSerial);
+
+    char userInput[BUFFER_SIZE];
+    DWORD bytesWritten;
 
     while (true) {
-        // Read data from the serial port
-        if (ReadFile(hSerial, buffer, BUFFER_SIZE, &bytesRead, nullptr)) {
-            if (bytesRead > 0) {
-                // Print received data to the console
-                std::cout.write(buffer, bytesRead);
-            }
-        }
-
-        // Read data from the user and send it to the serial port
-        std::string userInput;
         std::cout << "Enter data to send (or 'exit' to quit): ";
-        std::getline(std::cin, userInput);
+        std::cin.getline(userInput, BUFFER_SIZE);
 
-        if (userInput == "exit") {
+        if (strcmp(userInput, "exit") == 0) {
+            shouldExit = true;
             break;
         }
 
-        if (!WriteFile(hSerial, userInput.c_str(), userInput.length(), &bytesWritten, nullptr)) {
+        if (!WriteFile(hSerial, userInput, strlen(userInput), &bytesWritten, nullptr)) {
             std::cerr << "Error writing to serial port." << std::endl;
         }
     }
+
+    // Wait for the serial thread to finish
+    serialThread.join();
 
     CloseHandle(hSerial);
     return 0;
